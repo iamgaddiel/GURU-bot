42b04afc3175c94990e0cda662792735ee5fe2c1
Gaddiel <ighotagaddiel98@gmail.com>
2021-04-30 21:47:47 +0100

    modified clicked referal link counter

--- bot.py
+++ bot.py
@@ -3,25 +3,20 @@ import time
 from mechanicalsoup import StatefulBrowser
 import requests
 
-# time.sleep(2.00)
 
-
-def click_ref_link(username):
+def click_ref_link(username: str, counter: int):
     try:
-        counter: int = 1
         url: str = f"https://ref.moneyguru.co/{username}"
         browser = StatefulBrowser()
         browser.open(url)
-        
         if browser.url == "https://moneyguru.co":
             print(f'{counter}: clicked', url)
-
     except requests.exceptions.ConnectionError:
         print('You have network connection problem')
 
-
+counter: int = 1
 username: str = input('Enter your username: ')
 while True:
     time.sleep(1.00)
-    click_ref_link(username)
-    
+    click_ref_link(username, counter)
+    counter += 1
